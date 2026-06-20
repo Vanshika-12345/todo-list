9c50721a7b61d673a28cd795fadbaee61ba518ea
Vanshika-12345 <vanshikavanshika53@gmail.com>
2026-06-20 16:02:04 +0530

    added error handling

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -5,10 +5,11 @@ import { useForm } from "react-hook-form";
 const Create = (props) => {
   const todo = props.todo;
   const setTodo = props.setTodo;
-  const {register,
+  const {
+    register,
     handleSubmit,
     reset,
-    formState: {errors}
+    formState: { errors },
   } = useForm();
   const SubmitHandler = (data) => {
     data.isCompleted = false;
@@ -21,18 +22,24 @@ const Create = (props) => {
   };
   return (
     <div className="border-transparent w-full lg:w-3/5 p-10 text-center bg-[#4B5694] rounded-md">
-      <h1 className="text-5xl font-thin font-bold text-[#EAE0CF]">Create Tasks</h1>
+      <h1 className="text-5xl font-thin font-bold text-[#EAE0CF]">
+        Create Tasks
+      </h1>
       <form onSubmit={handleSubmit(SubmitHandler)}>
-       <input
-  {...register("title", { required: "Title is required" })}
-  className="w-full max-w-md mx-auto mt-10 mb-10 border-b px-4 py-3 rounded-sm text-lg font-semibold text-center uppercase"
-  type="text"
-  placeholder="Title"
-/>
+        <input
+          {...register("title", { required: "Title is required" })}
+          className="w-full max-w-md mx-auto mt-10 mb-2 border-b px-4 py-3 rounded-sm text-lg font-semibold text-center uppercase"
+          type="text"
+          placeholder="Title"
+        />
         <br />
-        <button
-        className="border-none px-12 py-2 bg-[#EAE0CF] text-[#111844] rounded-lg shadow-md hover:scale-105 cursor-pointer"
-        >Create Task</button>
+        {errors.title && (
+          <small className="text-red-800">{errors.title.message}</small>
+        )}
+        <br />
+        <button className="border-none px-12 py-2 mt-5 bg-[#EAE0CF] text-[#111844] rounded-lg shadow-md hover:scale-105 cursor-pointer">
+          Create Task
+        </button>
       </form>
     </div>
   );
