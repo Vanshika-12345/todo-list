c6d10d1501079ee006de3b38ff75bcb49eb311fb
Vanshika-12345 <vanshikavanshika53@gmail.com>
2026-06-22 17:24:12 +0530

    added changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import App from './App.jsx'
 import "./index.css"
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Wrapper from './Wrapper.jsx';
-createRoot(document.getElementById('root')).render(<Wrapper App={<App />} />)
-//    <>
-//     <App />
-//     <ToastContainer position="top-center"/>
-//     </>
+createRoot(document.getElementById('root')).render(
+    <>
+    <App />
+    <ToastContainer position="top-center"/>
+    </>
+)
